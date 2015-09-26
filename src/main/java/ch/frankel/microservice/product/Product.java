4c58af2f4baed064d3e9de007f1a43c4e5463178
Nicolas Frankel <nicolas@frankel.ch>
2015-09-26 23:46:12 +0200

    Initial commit

---
+++ src/main/java/ch/frankel/microservice/product/Product.java
@@ -0,0 +1,32 @@
+package ch.frankel.microservice.product;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private long id;
+
+    private String name;
+
+    public Product() {}
+
+    public Product(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
