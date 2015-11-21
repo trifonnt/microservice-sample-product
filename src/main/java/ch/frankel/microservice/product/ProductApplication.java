ee0b89c05ea952ceca2d280a1c69a9ffd3fef48c
trifonnt <trifont@gmail.com>
2015-11-21 18:05:26 +0200

    Code formating.

--- src/main/java/ch/frankel/microservice/product/ProductApplication.java
+++ src/main/java/ch/frankel/microservice/product/ProductApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductApplication {
 
     public static void main(String... args) {
-        SpringApplication.run(ProductApplication.class);
+        SpringApplication.run(ProductApplication.class, args);
     }
 }
